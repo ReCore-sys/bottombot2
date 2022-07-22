4bdd0847ccac61676eeb75be1f08ff75f87eb634
ReCore-sys <kjshore2004@gmail.com>
2022-07-22 23:29:00 +0930

    Revert "So vscode just chose not to tell me about that huh"
    This reverts commit ccbf2756873f51ac4a0d0f0ece37f6cacf374fd8.

--- libs/stocks/stocks.go
+++ libs/stocks/stocks.go
@@ -13,7 +13,8 @@ import (
 	"strings"
 	"time"
 
-	db "github.com/ReCore-sys/bottombot2/libs/database"
+	"github.com/ReCore-sys/bottombot2/libs/config"
+	mongo "github.com/ReCore-sys/bottombot2/libs/database"
 	"github.com/ReCore-sys/bottombot2/libs/logging"
 	"github.com/ReCore-sys/bottombot2/libs/utils"
 	"github.com/bwmarrin/discordgo"
@@ -49,6 +50,13 @@ func RegisterStocks(router *dgc.Router) *dgc.Router {
 		Usage:       "stocks <buy/sell> <ticker> <amount>",
 		Aliases:     []string{"stock", "stonks"},
 		Handler: func(ctx *dgc.Ctx) {
+			CFG := config.Config()
+			db, err := mongo.OpenSession(CFG.Server, CFG.DBPort, CFG.Collection) // Create a RavenDB session
+			if err != nil {
+				logging.Log(err)
+			}
+
+			defer db.Close()
 			var ticker string
 			args := utils.ParseArgs(ctx)
 			//users := db.GetAll()
@@ -57,7 +65,7 @@ func RegisterStocks(router *dgc.Router) *dgc.Router {
 				 *               Stock price
 				 *=============================================**/
 				response := "**Current stock prices:**\n\n"
-				sortedtickers := db.Tickers
+				sortedtickers := mongo.Tickers
 				sort.Strings(sortedtickers)
 				for _, ticker := range sortedtickers {
 					response += fmt.Sprintf("%s: $%.2f\n", ticker, Prices[ticker])
@@ -99,7 +107,7 @@ func RegisterStocks(router *dgc.Router) *dgc.Router {
 						logging.Log(err)
 					}
 					return
-				} else if !utils.IsIn(ticker, db.Tickers) {
+				} else if !utils.IsIn(ticker, mongo.Tickers) {
 					err = ctx.RespondText("That ticker is not valid.")
 					if err != nil {
 						logging.Log(err)
@@ -154,6 +162,7 @@ func RegisterStocks(router *dgc.Router) *dgc.Router {
 					if err != nil {
 						logging.Log(err)
 					}
+					db.Close()
 				case "sell":
 					user, err := db.Get(ctx.Event.Author.ID)
 					if err != nil {
@@ -232,7 +241,7 @@ func PriceLoop(discord *discordgo.Session) {
 		if utils.IntervalCheck(20 * 1000 * 60) {
 
 			UntilChange = time.Now().Add(20 * time.Minute)
-			for _, ticker := range db.Tickers {
+			for _, ticker := range mongo.Tickers {
 				Prices[ticker] = math.Round(GeneratePrice(ticker)*100) / 100
 			}
 			UpdatePricesFile(Prices)
